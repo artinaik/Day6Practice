4260b0725ff248315b1e75e8947fa7ce959df73e
Your UserName <aratinaik151997@gmail.com>
2021-11-22 15:48:05 +0530

    Added program for fibonoccie series

---
+++ LogicalPrograms/LogicalPrograms/Fibonocci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class Fibonoccies
+    {
+        public void FindSeries()
+        {
+            int firstNum = 0, secondNum = 1, nextNum = 0, i;
+            Console.WriteLine("Enter number of series");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write(""+firstNum);
+            Console.Write("+"+secondNum);
+            for (i = 2; i <num; i++)
+            {
+                nextNum = firstNum + secondNum;
+                Console.Write("+"+nextNum);
+                firstNum = secondNum;
+                secondNum = nextNum;
+            }
+        }
+    }
+}
---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Programs");
+            Fibonoccies fibonocciesobj = new Fibonoccies();
+            fibonocciesobj.FindSeries();
+            Console.ReadKey();
+        }
+    }
+}
