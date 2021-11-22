ea2716eac0c103b16f231475d67952cd2a11086b
Your UserName <aratinaik151997@gmail.com>
2021-11-22 18:06:14 +0530

    Added program to Check number is prime or not

---
+++ LogicalPrograms/LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class PrimeNumber
+    {
+        public void CheckPrime()
+        {
+            int num,i;
+            bool prime=true;
+            Console.WriteLine("Enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            for(i=2;i<=num/2;i++)
+            {
+                if (num % i == 0)
+                    prime = false;
+            }
+            if(prime==true)
+            {
+                Console.WriteLine("Number is prime number");
+            }
+            else
+            {
+                Console.WriteLine("Number is not a prime number");
+            }
+
+        }
+    }
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -12,7 +12,7 @@ namespace LogicalPrograms
         {
             int choice;
             Console.WriteLine("Logical Programs");
-            Console.WriteLine("1)Fibonoccie Series 2)Perfect Number");
+            Console.WriteLine("1)Fibonoccie Series 2)Perfect Number 3)Prime Number");
             Console.WriteLine("Enter your choice");
             choice = Convert.ToInt32(Console.ReadLine());
             switch(choice)
@@ -24,6 +24,10 @@ namespace LogicalPrograms
                 case 2:
                     PerfectNumber perfectNumberobj = new PerfectNumber();
                     perfectNumberobj.CheckPerfectNumber();
+                    break;
+                case 3:
+                    PrimeNumber primeNumberobj = new PrimeNumber();
+                    primeNumberobj.CheckPrime();
                     break;
             }
             
